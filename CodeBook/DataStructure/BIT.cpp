a2f2a219e49f9f67342ff7cc81049cadaef9ecf9
marmot0814 <s1357andy871207@gmail.com>
2019-08-15 20:48:09 +0800

    SparseTable and BIT

---
+++ CodeBook/DataStructure/BIT.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+// LatexBegin
+struct BIT {
+  vector<int> data; int n;
+  BIT(vector<int> &arr) {
+    n = (int)arr.size();
+    data.clear(); data.resize(n + 1, 0);
+    for (int i = 0 ; i < n ; i++)
+      modify(i, arr[i]);
+  }
+  int lowbit(int x) { return x & -x; }
+  int query(int x) { x++;
+    int ret = 0;
+    while (x > 0) ret += data[x], x -= lowbit(x);
+    return ret;
+  }
+  void modify(int x, int d) { x++;
+    while (x <= n) data[x] += d, x += lowbit(x);
+  }
+};
+// LatexEnd
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  int t, kase = 0; cin >> t; while (t--){
+    cout << "Case " << ++kase << ":\n";
+    int n; cin >> n;
+    vector<int> data(n);
+    for (auto &v : data)
+      cin >> v;
+    BIT *sol = new BIT(data);
+    string op; while (cin >> op){
+      int a, b;
+      if (op[0] == 'E') break;
+      if (op[0] == 'Q'){
+        cin >> a >> b; a--, b--;
+        cout << sol->query(b) - sol->query(a - 1) << '\n';
+      }
+      if (op[0] == 'A'){
+        cin >> a >> b; a--;
+        sol->modify(a, b);
+      }
+      if (op[0] == 'S'){
+        cin >> a >> b; a--;
+        sol->modify(a, -b);
+      }
+    }
+  }
+}
---
+++ CodeBook/DataStructure/SparseTable.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+// LatexBegin
+struct SparseTable{
+  vector<vector<int> > data;
+  int (*op)(int a, int b);
+  SparseTable(vector<int> &arr, int (*_op)(int a, int b)) {
+    op = _op;
+    int n = (int)arr.size(), lgN = __lg(n) + 1;
+    data.resize(lgN);
+    for (int i = 0 ; i < n ; i++)
+      data[0].push_back(arr[i]);
+    for (int h = 1 ; h < lgN ; h++){
+      int len = 1 << (h - 1), i = 0;
+      for (; i + len < n ; i++)
+        data[h].push_back(op(data[h-1][i], data[h-1][i+len]));
+      if (!i) break;
+      for (; i < n ; i++)
+        data[h].push_back(data[h-1][i]);
+    }
+  }
+  int query(int l, int r){
+    int h = __lg(r - l);
+    int len = 1 << h;
+    return op(data[h][l], data[h][r-len]);
+  }
+};
+// LatexEnd
+int getMin(int a, int b) {
+  return min(a, b);
+}
+int getMax(int a, int b) {
+  return max(a, b);
+}
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  int n, q; cin >> n >> q;
+  vector<int> arr(n);
+  for (auto &v : arr)
+    cin >> v;
+  SparseTable *minV = new SparseTable(arr, getMin);
+  SparseTable *maxV = new SparseTable(arr, getMax);
+  while (q--) {
+    int l, r; cin >> l >> r; l--;
+    cout << maxV->query(l, r) - minV->query(l, r) << endl;
+  }
+  delete minV;
+  delete maxV;
+}
