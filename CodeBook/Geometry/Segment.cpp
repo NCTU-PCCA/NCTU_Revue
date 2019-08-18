851bfd7ceeb50f65f056564d265ea347f0f9c6e1
marmot0814 <s1357andy871207@gmail.com>
2019-08-18 20:33:14 +0800

    Segment

--- CodeBook/Geometry/Segment.cpp
+++ CodeBook/Geometry/Segment.cpp
@@ -16,7 +16,6 @@ T leng2(V a) { return a * a; }
 T leng(V a) { return sqrt(leng2(a)); }
 T dist2(P a, P b) { return leng2(a - b); }
 T dist(P a, P b) { return sqrt(dist2(a, b)); }
-// LatexBegin
 struct L { P p1, p2; L() {}
     L(P p1, P p2) : p1(p1), p2(p2) {}
 };
@@ -40,15 +39,46 @@ int intersect(L l1, L l2) {
   return parallel(l1, l2) ? 
       (dir(l1.p2, l2) == 0 ? -1 : 0) : 1;
 }
+// LatexBegin
+struct S { P p1, p2; S() {}
+  S(P p1, P p2) : p1(p1), p2(p2) {}
+};
+T dist(P p, S s) {
+  V v = s.p2 - s.p1, v1 = p - s.p1, v2 = p - s.p2;
+  if (v * v1 < 0) return leng(v1);
+  if (v * v2 > 0) return leng(v2);
+  return abs(v1 % v2) / leng(v);
+}
+T btw(P p, S s) { return (s.p1 - p) * (s.p2 - p); }
+T dir(P p, S s) { return dir(p, L{s.p1, s.p2}); }
+bool intersect(P p, S s) {
+  return dir(p, s) == 0 && btw(p, s) <= 0;
+}
+bool intersection(S s1, S s2) {
+  T c1 = dir(s2.p1, s1), c2 = dir(s2.p2, s1);
+  T c3 = dir(s1.p1, s2), c4 = dir(s1.p2, s2);
+  if (c1 * c2 < 0 && c3 * c4 < 0) return true;
+  if (c1 == 0 && intersect(s2.p1, s1)) return true;
+  if (c2 == 0 && intersect(s2.p2, s1)) return true;
+  if (c3 == 0 && intersect(s1.p1, s2)) return true;
+  if (c4 == 0 && intersect(s1.p2, s2)) return true;
+  return false;
+}
+T dist(S s1, S s2) { 
+  if (intersection(s1, s2)) return 0;
+  return min({
+    dist(s1.p1, s2), dist(s1.p2, s2),
+    dist(s2.p1, s1), dist(s2.p2, s1)
+  });
+}
 // LatexEnd
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
-  L l1, l2;
-  cin >> l1.p1.x >> l1.p1.y;
-  cin >> l1.p2.x >> l1.p2.y;
-  cin >> l2.p1.x >> l2.p1.y;
-  cin >> l2.p2.x >> l2.p2.y;
-  cout << fixed << setprecision(10) << dist(l1, l2) << '\n';
-  cout << parallel(l1, l2) << '\n';
-  cout << intersect(l1, l2) << '\n';
+  S s1, s2; 
+  cin >> s1.p1.x >> s1.p1.y;
+  cin >> s1.p2.x >> s1.p2.y;
+  cin >> s2.p1.x >> s2.p1.y;
+  cin >> s2.p2.x >> s2.p2.y;
+  cout << intersection(s1, s2) << '\n';
+  cout << fixed << setprecision(10) << dist(s1, s2) << '\n';
 }
