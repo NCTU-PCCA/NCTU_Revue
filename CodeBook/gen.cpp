6cd559d09f5c841ff7df9b18e642a125a68dcdfd
marmot0814 <s1357andy871207@gmail.com>
2019-08-25 15:14:56 +0800

    check latex label

--- CodeBook/gen.cpp
+++ CodeBook/gen.cpp
@@ -4,14 +4,22 @@
 #define ERROR "\033[1;31m"
 #define CORRECT "\033[1;32m"
 #define INFO "\033[1;34m"
+#define WARNING "\033[1;33m"
 #define END   "\033[0m"
 using namespace std;
+int cmd(const string &s) { return system(s.c_str()); }
 void throwParserError(const string &msg, int line_cnt, string &input) {
     cerr << ERROR << LINE << msg << ": " << input << '\n' << END;
     exit(1);
 }
 void throwTestError(const string &msg) {
     cerr << ERROR << msg << '\n' << END;
+    exit(1);
+}
+void throwCheckLatexLabelError(const string &sour_path, int line_cnt, const string &msg) {
+    cerr << ERROR << sour_path;
+    if (line_cnt) cerr << LINE;
+    cerr << ": " << msg << '\n' << END;
     exit(1);
 }
 bool isPathExist(const string &s) {
@@ -91,9 +99,6 @@ struct CodeBook : vector<S> {
         }
     }
 };
-int cmd(const string &s) {
-    return system(s.c_str());
-}
 void testOne(s &_s) {
     if (cmd("g++ -std=c++17 " + _s.test_sour_path + " -o test.out"))
         throwTestError("Compile " + _s.test_sour_path + " Error");
@@ -115,7 +120,7 @@ void testOne(s &_s) {
         if (wa) {
             cout << ERROR << "WA\n" << END;
             cmd("rm result test.out");
-            throwTestError(_s.name + ": wrong answer on test " + str);
+            throwTestError(_s.name + ": wrong answer on test " + str + ".");
         } else cout << CORRECT << "AC\n" << END;
     }
     cmd("rm result test.out");
@@ -139,6 +144,29 @@ void testAll(CodeBook &codebook, bool gitDiffCheck = true) {
         }
     }
 }
+void checkLatexLabel(CodeBook &codebook) {
+    for (auto &_S : codebook) {
+        for (auto &_s : _S) {
+            ifstream code(_s.sour_path);
+            string str; int write = 0, line_cnt = 0;
+            while (getline(code, str)) {
+                line_cnt++;
+                if (str == "// LatexEnd") {
+                    if (write % 2 == 0)
+                        throwCheckLatexLabelError(_s.sour_path, line_cnt, "Latex Label \"// LatexEnd\" must have a \"// LatexBegin\" in front of it");
+                    write++;
+                }
+                if (str == "// LatexBegin") {
+                    if (write % 2 == 1)
+                        throwCheckLatexLabelError(_s.sour_path, line_cnt, "Latex Label \"// LatexBegin\" must not after another \"// LatexBegin\"");
+                    write++;
+                }
+            }
+            if (!write)
+                throwCheckLatexLabelError(_s.sour_path, 0, "Latex Label Not Found");
+        }
+    }
+}
 void genCodeBook(CodeBook &codebook) {
     cmd("mkdir .tmp");
     ofstream out("content.tex");
@@ -158,29 +186,34 @@ void genCodeBook(CodeBook &codebook) {
                     write = true;
             }
             out << "\t\\subsection{" << _s.name << "}\n";
-            out << "\t\t\\lstinputlisting [language=c++] { .tmp/" << _s.sour_path << "}\n";
+            out << "\t\t\\lstinputlisting [language=c++] { .tmp/" << _s.sour_path << " }\n";
         }
     }
 }
 int main(int argc, char **argv) {
     string op = (argc >= 2 ? string(argv[1]) : "genCodeBook");
     ifstream config("config");
-    cout << INFO << "Config Parser\n" << END;
+    cout << INFO << "Config Parser.\n" << END;
     CodeBook codebook(config);
-    cout << CORRECT << "Config Parser Correct\n" << END;
+    cout << CORRECT << "Config Parser Correct.\n" << END;
     if (op == "genCodeBook" || op == "test" || op == "check") {
-        cout << INFO << "Test Template\n" << END;
+        cout << INFO << "Test Template.\n" << END;
         testAll(codebook, op != "test");
-        cout << CORRECT << "Test Template Correct\n" << END;
+        cout << CORRECT << "Test Template Correct.\n" << END;
+    }
+    if (op == "genCodeBook" || op == "check") {
+        cout << INFO << "Check Latex Label.\n" << END;
+        checkLatexLabel(codebook);
+        cout << CORRECT << "Check Latex Label Done.\n" << END;
     }
     if (op == "genCodeBook") {
-        cout << INFO << "Generate CodeBook" << END << '\n';
+        cout << INFO << "Generate CodeBook." << END << '\n';
         genCodeBook(codebook);
         cmd("xelatex codebook.tex");
         cmd("xelatex codebook.tex");
         cmd("rm codebook.aux codebook.log codebook.toc -f > /dev/null");
         cmd("rm -rf .tmp");
         cmd("rm content.tex");
-        cout << CORRECT << "Generate CodeBook Done\n" << END;
+        cout << CORRECT << "Generate CodeBook Done.\n" << END;
     }
 }
