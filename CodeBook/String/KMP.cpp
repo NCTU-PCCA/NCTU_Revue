d53551fd50a422ddecbd20a33942c6ccd7e572ea
marmot0814 <s1357andy871207@gmail.com>
2019-08-25 10:22:22 +0800

    add KMP

---
+++ CodeBook/String/KMP.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> build(string &t) {
+  vector<int> F(t.size()); F[0] = -1;
+  for (int i = 1, pos = -1 ; i < t.size() ; i++) {
+    while (~pos && t[i] != t[pos + 1]) pos = F[pos];
+    if (t[i] == t[pos + 1]) pos++;
+    F[i] = pos;
+  }
+  return F;
+}
+bool match(string &s, string &t, vector<int> &F) {
+  for (int i = 0, pos = -1 ; i < s.size() ; i++) {
+    while (~pos && s[i] != t[pos + 1]) pos = F[pos];
+    if (s[i] == t[pos + 1]) pos++;
+    if (pos + 1 == (int)t.size()) return true;
+  }
+  return false;
+}
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  string s, t; cin >> t;
+  vector<int> F = build(t);
+  while (cin >> s)
+    cout << match(s, t, F) << '\n';
+}
