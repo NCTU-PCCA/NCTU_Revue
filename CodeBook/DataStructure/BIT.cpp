de95e6738bb41ca90a0c88c129b4aa9313222689
marmot0814 <s1357andy871207@gmail.com>
2019-08-26 22:34:24 +0800

    add Interval Update and Interval Query BIT

--- CodeBook/DataStructure/BIT.cpp
+++ CodeBook/DataStructure/BIT.cpp
@@ -1,47 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long LL;
 // LatexBegin
-struct BIT {
-  vector<int> data; int n;
-  BIT(int n) : n(n) {
-    data.clear(); data.resize(n + 1, 0);
-  }
-  int lowbit(int x) { return x & -x; }
-  int query(int x) { x++;
-    int ret = 0;
-    while (x > 0) ret += data[x], x -= lowbit(x);
+struct BIT { vector<LL> d1, d2; int n;
+  BIT(int n) : n(n) { d1.resize(n + 1, 0); d2 = d1; }
+  void _(int &x, int v) { x += v * (x & -x); }
+  int Q(int x) { x++; LL ret = 0, p = x;
+    while (x > 0) ret += (p+1)*d1[x] - d2[x], _(x,-1);
     return ret;
   }
-  void modify(int x, int d) { x++;
-    while (x <= n) data[x] += d, x += lowbit(x);
+  void M(int x, LL v) { x++; LL p = x;
+    while (x <= n) d1[x] += v, d2[x] += v * p, _(x, 1);
   }
+  int Q(int l, int r) { return Q(r - 1) - Q(l - 1); }
+  void M(int l, int r, LL v) { M(l, v); M(r, -v); }
 };
 // LatexEnd
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
-  int t, kase = 0; cin >> t; while (t--){
-    cout << "Case " << ++kase << ":\n";
-    int n; cin >> n;
-    BIT *sol = new BIT(n);
+  int t; cin >> t; while (t--) {
+    int n; cin >> n; BIT *sol = new BIT(n);
     for (int i = 0 ; i < n ; i++) {
-      int v; cin >> v;
-      sol->modify(i, v);
+      LL v; cin >> v; sol->M(i, i + 1, v);
     }
-    string op; while (cin >> op){
-      int a, b;
-      if (op[0] == 'E') break;
-      if (op[0] == 'Q'){
-        cin >> a >> b; a--, b--;
-        cout << sol->query(b) - sol->query(a - 1) << '\n';
+    int q; cin >> q; while (q--) {
+      int op; cin >> op;
+      if (op == 0) { // Q
+        int l, r; cin >> l >> r;
+        cout << sol->Q(l, r) << '\n';
       }
-      if (op[0] == 'A'){
-        cin >> a >> b; a--;
-        sol->modify(a, b);
-      }
-      if (op[0] == 'S'){
-        cin >> a >> b; a--;
-        sol->modify(a, -b);
+      if (op == 1) {
+        int l, r; LL v; cin >> l >> r >> v;
+        sol->M(l, r, v);
       }
     }
+    delete sol;
   }
 }
