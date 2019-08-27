2714586c43230f940271f857dd41bed68e0c01c3
marmot0814 <s1357andy871207@gmail.com>
2019-08-27 12:17:05 +0800

    CRT indentation

--- CodeBook/Math/CRT.cpp
+++ CodeBook/Math/CRT.cpp
@@ -7,10 +7,10 @@ typedef pair<LL, LL> pLL;
 // LatexBegin
 pLL CRT(vector<LL> &A, vector<LL> &B, vector<LL> &M) {
   // A[i]x = B[i] (mod M[i]); F : ans, S : lcm of M;
-  LL x = 0, m = 1; for (int i = 0 ; i < A.size() ; i++) {
+  LL x = 0, m = 1; for (int i = 0; i < A.size(); i++) {
     LL a = A[i] * m, b = B[i] - A[i] * x;
-    LL d = __gcd(M[i], a); if (b % d) return pLL(0, -1);
-    LL t = b / d * modInv(a / d, M[i] / d) % (M[i] / d);
+    LL d = __gcd(M[i], a); if (b % d) return pLL(0,-1);
+    LL t = b / d * modInv(a / d, M[i] / d) % (M[i]/d);
     x = x + m * t; m *= M[i] / d;
   }
   return x = (x % m + m) % m, pLL(x, m);
