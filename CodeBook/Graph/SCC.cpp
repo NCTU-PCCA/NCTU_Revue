a5bf35d8b17ff306ba491c7b59e5b9d246f4d831
marmot0814 <s1357andy871207@gmail.com>
2020-03-20 20:19:58 +0800

    SCC depointer

--- CodeBook/Graph/SCC.cpp
+++ CodeBook/Graph/SCC.cpp
@@ -2,50 +2,46 @@
 using namespace std;
 const int MAXN = 1e5 + 5;
 // LatexBegin
-struct Graph { int V, stamp, scc_num; 
-  struct Node : vector<Node*> { int dfn, low, scc, inS;
-    Node() { dfn = low = scc = -1; inS = 0; clear(); }
-  }_memN[MAXN], *node[MAXN];
-  Graph(int V) : V(V) {
-    for (int i = 0 ; i < V ; i++) node[i] = _memN + i;
+struct Graph { int stamp, scc_num;
+  struct Node : vector<int> { 
+    int dfn, low, scc; bool inS;
+    Node() { dfn = low = scc = -1; inS = false; }
+  };
+  vector<Node> N; vector<int> stk;
+  void init(int n) { 
+    N.clear(); N.resize(n); stk.clear();
   }
-  void addEdge(int u, int v) {
-    node[u]->push_back(node[v]);
-  }
-  int findSCC() {
-    stamp = scc_num = 0;
-    for (int i = 0 ; i < V ; i++)
-      if (!~node[i]->dfn)
-        Tarjan(node[i]);
-    return scc_num;
-  }
-  stack<Node*> stk;
-  void Tarjan(Node *u) { u->dfn = u->low = stamp++; 
-    stk.push(u); u->inS = true;
-    for (auto &v : *u) if (!~v->dfn) Tarjan(v),
-        u->low = min(u->low, v->low);
-      else if (v->inS)
-        u->low = min(u->low, v->dfn);
-    if (u->dfn == u->low) {
-      Node *v; do { v = stk.top(); stk.pop();
-        v->scc = scc_num; v->inS = false;
+  void addEdge(int u, int v) { N[u].push_back(v); }
+  void Tarjan(int u) { N[u].low = N[u].dfn = stamp++;
+    stk.push_back(u); N[u].inS = true;
+    for (auto &v : N[u]) if (!~N[v].dfn) Tarjan(v),
+      N[u].low = min(N[u].low, N[v].low);
+    else if (N[v].inS)
+      N[u].low = min(N[u].low, N[v].dfn);
+    if (N[u].dfn == N[u].low) {
+      int v; do { v = stk.back(); stk.pop_back();
+        N[v].scc = scc_num; N[v].inS = false;
       } while (v != u); scc_num++;
     }
   }
-};
+  int findSCC() { stamp = scc_num = 0;
+    for (int i = 0 ; i < N.size() ; i++)
+      if (!~N[i].dfn) Tarjan(i);
+    return scc_num;
+  }
+} G;
 // LatexEnd
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
   int n, m; while (cin >> n >> m && n) {
-    Graph *sol = new Graph(n);
+    G.init(n);
     while (m--) {
       int u, v; cin >> u >> v; u--, v--;
-      sol->addEdge(u, v);
+      G.addEdge(u, v);
     }
-    if (sol->findSCC() == 1)
+    if (G.findSCC() == 1)
       cout << "Yes\n";
     else 
       cout << "No\n";
-    delete sol;
   }
 }
