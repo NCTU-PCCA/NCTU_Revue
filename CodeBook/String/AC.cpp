e145eaed0a5f0768038a997bf2d7aae7807bbe8b
marmot0814 <s1357andy871207@gmail.com>
2020-07-16 16:24:56 +0800

    shorten AC

---
+++ CodeBook/String/AC.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+using namespace std;
+// LatexBegin
+struct AC { struct Node; vector<Node> N; 
+// LatexEnd
+/*
+// LatexBegin
+  struct Node : map<char, int> { int p; }; int r;
+// LatexEnd
+*/
+  struct Node : map<char, int> { int p, dp, vis; }; int r;
+// LatexBegin
+  void init() { N.PB({{}}); r = newNode(); }
+  int newNode() { N.PB({}); return N.size() - 1; }
+  void addTrie(string &s) { int u = r;
+    for (auto &c : s) { if (!N[u].count(c)) 
+      N[u][c] = newNode(); u = N[u][c];
+    }
+// LatexEnd
+    N[u].dp++;
+// LatexBegin
+  }
+  void buildAC() { static queue<int> q; q.push(r);
+    while (q.size()) { int u = q.front(); q.pop();
+      for (auto &p : N[u])
+        N[p.S].p = trans(N[u].p, p.F), q.push(p.S);
+    }
+  }
+  int trans(int u, char c) { if (!u) return r;
+    if (!N[u].count(c)) N[u][c] = trans(N[u].p, c);
+    return N[u][c];
+  }
+} solver;
+// LatexEnd
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  int t; cin >> t; while (t--) {
+    solver.init();
+    int n; cin >> n; while (n--) {
+      string s; cin >> s; solver.addTrie(s);
+    }
+    solver.buildAC();
+    string s; cin >> s;
+    auto &N = solver.N; int u = solver.r, ans = 0;
+    for (auto &c : s) {
+      u = solver.trans(u, c);
+      int p = u; while (p && !N[p].vis) {
+        ans += N[p].dp;
+        N[p].vis = 1;
+        p = N[p].p;
+      }
+    }
+    cout << ans << '\n';
+  }
+}
