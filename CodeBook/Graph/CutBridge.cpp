c0309b261c3498220b61d17d5758ac1773679e54
marmot0814 <s1357andy871207@gmail.com>
2020-08-31 13:29:42 +0800

    Update Cut Bridge

--- CodeBook/Graph/CutBridge.cpp
+++ CodeBook/Graph/CutBridge.cpp
@@ -1,74 +1,60 @@
 #include <bits/stdc++.h>
+#define PB push_back
+#define F first
+#define S second
 using namespace std;
-const int MAXN = 1e4 + 5;
-set<pair<int, int> > ans;
+typedef pair<int, int> pii;
 // LatexBegin
-struct Graph { int V, stamp;
-  struct Node : vector<Node*> {
-    int low, dfn; bool is_cut; Node *pa;
-    Node() { low = dfn = -1;
-      is_cut = false; pa = NULL;
-    }
-  }_memN[MAXN], *node[MAXN];
-  Graph (int V) : V(V) { stamp = 0;
-    for (int i = 0 ; i < V ; i++)
-      node[i] = _memN + i;
+struct Graph { int st, n;
+  struct Node : vector<int> { int low, dfn, p; };
+  vector<Node> N;
+  void init(int _n) { N.assign(n = _n, {});}
+  void addEdge(int u, int v) {N[u].PB(v);N[v].PB(u);}
+  void Tarjan(int u, int p) {
+    N[u].p = p; N[u].dfn = N[u].low = ++st;
+    for (auto &v : N[u]) if (!N[v].dfn) Tarjan(v, u),
+      N[u].low = min(N[u].low, N[v].low);
+    else if (p != v) N[u].low = min(N[u].low,N[v].dfn);
   }
-  void addEdge(int u, int v) {
-    node[u]->push_back(node[v]);
-    node[v]->push_back(node[u]);
+  void run() { for (int i = 0, st = 0 ; i < n ; i++)
+      if (!N[i].dfn) Tarjan(i, i);
   }
-  void Tarjan(Node *u, Node *pa) {
-    u->pa = pa; u->dfn = u->low = ++stamp;
-    for (auto &v : *u) if (!~v->dfn)
-      Tarjan(v, u), u->low = min(u->low, v->low);
-    else if (pa != v)
-      u->low = min(u->low, v->dfn);
+  set<int> cuts() { set<int> ret; map<int, int> rt;
+    for (int i = n - 1 ; ~i ; i--) { int p = N[i].p;
+      if (p == i) { if (rt[i] > 1) ret.insert(i); 
+      } else if (p == N[p].p) rt[p]++;
+      else if (N[i].low >= N[p].dfn) ret.insert(p);
+    }
+    return ret;
   }
-  void CutBridge() { int rt_son = 0;
-    Tarjan(node[0], NULL);
-    for (int i = 1 ; i < V ; i++) {
-      Node *pa = node[i]->pa;
-      if (pa == node[0]) rt_son++;
-      else if (node[i]->low >= pa->dfn)
-        pa->is_cut = true;
-    }
-    if (rt_son > 1) node[0]->is_cut = true;
-    for (int i = 0 ; i < V ; i++)
-      if (node[i]->is_cut)
-        /* node[i] is a cut */;
-    for (int i = 0 ; i < V ; i++) {
-      Node *pa = node[i]->pa;
-      if (pa && node[i]->low > pa->dfn)
-        /* pa and node[i] is a bridge */;
-    }
-// LatexEnd
-    for (int i = 0 ; i < V ; i++) {
-      Node *pa = node[i]->pa;
-      if (pa && node[i]->low > pa->dfn) {
-        pair<int, int> e = {pa - _memN, node[i] - _memN};
-        if (e.first > e.second)
-          swap(e.first, e.second);
-        ans.insert(e);
-      }
-    }
-// LatexBegin
+  set<pii> bridges() { set<pii> ret;
+    for (int i = 0, p ; i < n ; i++)
+      if ((p = N[i].p) != i && N[i].low > N[p].dfn)
+        ret.insert({min(p, i), max(p, i)});
+    return ret;
   }
 };
 // LatexEnd
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(0);
   int n, m; while (cin >> n >> m && n) {
-    Graph *sol = new Graph(n); ans.clear();
+    Graph solver; solver.init(n);
     while (m--) {
       int u, v; cin >> u >> v;
-      sol->addEdge(u, v);
+      solver.addEdge(u, v);
     }
-    sol->CutBridge();
-    cout << ans.size();
-    for (auto &p : ans)
-      cout << ' ' << p.first << ' ' << p.second;
+    solver.run();
+
+    auto cut = solver.cuts();
+    cout << cut.size();
+    for (auto &v : cut)
+      cout << ' ' << v;
     cout << '\n';
-    delete sol;
+
+    auto bridge = solver.bridges();
+    cout << bridge.size();
+    for (auto &p : bridge)
+      cout << ' ' << p.F << ' ' << p.S;
+    cout << '\n';
   }
 }
