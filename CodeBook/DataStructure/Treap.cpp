29ebb6f0ad975b99dd3ba1fa6c5d34ca99440dc8
marmot0814 <s1357andy871207@gmail.com>
2020-09-07 22:48:48 +0800

    Update Treap

--- CodeBook/DataStructure/Treap.cpp
+++ CodeBook/DataStructure/Treap.cpp
@@ -1,193 +1,83 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long LL;
-#define F first
-#define S second
-const int MAXN = 2e5 + 5;
-const int MAXVER = MAXN;
-template <typename T> struct _ptrCntr{
-    T v; int c;
-    _ptrCntr(const T& _v):v(_v){ c = 0;}
-};
-template <typename T>
-struct Sptr{
-    _ptrCntr<T> *p;
-    T* operator->(){ return &p->v; }
-    T& operator* (){ return  p->v; }
-    operator _ptrCntr<T>*(){ return p; }
-    Sptr& operator = (const Sptr<T>& t){
-        if (p && !--p->c) delete p;
-        (p = t.p) && ++p->c;
-        return *this;
-    }
-    Sptr(_ptrCntr<T> *t = 0) : p(t){ p && ++p->c; }
-    Sptr(const Sptr& t) : p(t.p){ p  && ++p->c; }
-    ~Sptr(){ if (p && !--p->c) delete p;}
-};
-
+const int MAXN = 1e5 + 5;
 // LatexBegin
-//<<<<<<<<<<PERSISTENT
-#define PTR Sptr<Node>
-//====================
-// LatexEnd
-int d;
-/*
-// LatexBegin
-#define PTR Node*
-// LatexEnd
-*/
-// LatexBegin
-//>>>>>>>>>>ORIGIN
-#define PNN pair<PTR, PTR>
 struct Treap {
-  struct Node { PTR l; PTR r; int sz; char c;
-    Node (char c = 0) : c(c), l(NULL), r(NULL) {
-      sz = 1;
+  struct Node { Node *l, *r; int sz; char v; bool rev;
+    Node (char v = 0) : v(v), sz(1), l(NULL),
+      r(NULL), rev(false) {}
+  } mem[MAXN * 80], *r, *ptr;
+  void init(string s) { r = NULL; ptr = mem;
+    for (auto &c : s) r = merge(r,new (ptr++) Node(c));
+  }
+  int size(Node *u) { return u ? u->sz : 0; }
+  Node* merge(Node *a, Node *b) {
+    if (!a || !b) return a ? a : b; Node *t;
+    if (rand() % (size(a) + size(b)) < size(a))
+      // t = push(a),
+      t = new (ptr++) Node(*push(a)),
+      t->r = merge(a->r, b);
+    else
+      // t = push(b),
+      t = new (ptr++) Node(*push(b)),
+      t->l = merge(a, b->l);
+    return pull(t);
+  }
+  void split(Node *t, int k, Node *&a, Node *&b) {
+    if (!t) a = b = NULL;
+    else if (size(push(t)->l) < k)
+      // a = t,
+      a = new (ptr++) Node(*t),
+      split(t->r, k - size(t->l) - 1, a->r, b),pull(a);
+    else
+      // b = t,
+      b = new (ptr++) Node(*t),
+      split(t->l, k, a, b->l), pull(b);
+  }
+  Node* push(Node *&u) { if (!u) return u;
+    if (u->rev) { swap(u->l, u->r); u->rev = false;
+      if (u->l)
+        (u->l = new (ptr++) Node(*(u->l)))->rev ^= 1;
+      if (u->r)
+        (u->r = new (ptr++) Node(*(u->r)))->rev ^= 1;
     }
-  };
-  vector<PTR> rt;
-  Treap() { rt.resize(rt.size() + 1, NULL); }
-//<<<<<<<<<<PRESISTENT
-//====================
-// LatexEnd
-/*
-// LatexBegin
-  ~Treap() { clear(rt.back()) }
-  void clear(PTR u) { 
-    if (u) clear(u->l), clear(u->r), delete u;
+    return u;
   }
-// LatexEnd
-*/
-// LatexBegin
-//>>>>>>>>>>ORIGIN
-  inline PTR _new(const Node &u) {
-//<<<<<<<<<<PERSISTENT
-    return PTR(new _ptrCntr<Node>(u));
-//===================
-// LatexEnd
-/*
-// LatexBegin
-    return new Node(u.v);
-// LatexEnd
-*/
-// LatexBegin
-//>>>>>>>>>>ORIGIN
-  }
-  inline int size(PTR &u) { return u ? u->sz : 0; }
-  inline PTR& pull(PTR &u) {
+  Node* pull(Node *&u) { if (!u) return u;
     u->sz = 1 + size(push(u->l)) + size(push(u->r));
-    // pull function
     return u;
   }
-  inline PTR& push(PTR &u) {
-    if (!u) return u;
-    // push function
-    return u;
+  void print(Node *u) { if (!push(u)) return ;
+    print(u->l); cout << u->v; print(u->r);
   }
-  PNN split(PTR &T, int x) {
-    if (!T) return {(PTR)NULL, (PTR)NULL};
-//<<<<<<<<<<PRESISTENT
-    Sptr<Node> res = _new(*T);
-    if (size(T->l) < x){
-      PNN tmp = split(T->r, x - 1 - size(T->l));
-      res->r = tmp.F; return {pull(res), tmp.S};
-    } else {
-      PNN tmp = split(T->l, x);
-      res->l = tmp.S; return {tmp.F, pull(res)};
-    }
-//====================
-// LatexEnd
-/*
-// LatexBegin
-    if (size(push(T)->l) < x) {
-      PNN tmp = split(T->r, x - size(T->l) - 1);
-      T->r = tmp.F; return {pull(T), tmp.S};
-    } else {
-      PNN tmp = split(T->l, x);
-      T->l = tmp.S; return {tmp.F, pull(T)};
-    }
-// LatexEnd
-*/
-// LatexBegin
-//>>>>>>>>>>ORIGIN
+  void print(int L, int R) { Node *a, *b, *c;
+    split(r, R, a, c); split(a, L, a, b);
+    print(b); cout << '\n'; r = merge(merge(a, b), c);
   }
-  PTR merge(PTR &T1, PTR &T2) {
-    if (!T1 || !T2) return T1 ? T1 : T2;
-//<<<<<<<<<<PRESISTENT
-    Sptr<Node> res;
-    if (rand() % (size(T1) + size(T2)) < size(T1))
-      res = _new(*T1), res->r = merge(T1->r, T2);
-    else
-      res = _new(*T2), res->l = merge(T1, T2->l);
-    return pull(res);
-//====================
-// LatexEnd
-/*
-// LatexBegin
-    if (rand() % (size(T1) + size(T2)) < size(T1))
-      return T1->r = merge(push(T1)->r, T2), pull(T1);
-    else
-      return T2->l = merge(T1, push(T2)->l), pull(T2);
-// LatexEnd
-*/
-// LatexBegin
-//>>>>>>>>>>ORIGIN
+  void copy(int L, int R) { Node *a, *b, *c;
+    split(r, R, a, c); split(a, L, a, b);
+    r = merge(merge(a, b), merge(b, c));
   }
+  void reverse(int L, int R) { Node *a, *b, *c;
+    split(r, R, a, c); split(a, L, a, b);
+    b->rev ^= 1; r = merge(merge(a, b), c);
+  }
+} sol;
 // LatexEnd
-    void insert(char *s, int p){
-        rt.resize(rt.size() + 1, rt.back());
-        PNN tmp = split(rt.back(), p);
-        for (int i = 0 ; s[i] ; i++){
-            Sptr<Node> target = _new(Node(s[i]));
-            tmp.F = merge(tmp.F, target);
-        }
-        rt.back() = merge(tmp.F, tmp.S);
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  int t; cin >> t; while (t--) {
+    int n, q; cin >> n >> q;
+    string s; cin >> s;
+    sol.init(s);
+    while (q--) {
+      int op, l, r; cin >> op >> l >> r; l--;
+      if (op == 1)
+        sol.print(l, r);
+      else if (op == 2)
+        sol.copy(l, r);
+      else 
+        sol.reverse(l, r);
     }
-    void remove(int p, int c){
-        rt.resize(rt.size() + 1, rt.back());
-        PNN tmp_1 = split(rt.back(), p-1);
-        PNN tmp_2 = split(tmp_1.S, c);
-        rt.back() = merge(tmp_1.F, tmp_2.S);
-    }
-    void query(int v, int p, int c){
-        PNN tmp_1 = split(rt[v], p-1);
-        PNN tmp_2 = split(tmp_1.S, c);
-        Print(tmp_2.F); cout << '\n';
-    }
-    void Print(PTR &u){
-        if (!u) return ;
-        Print(u->l);
-        cout << u->c;
-        if (u->c == 'c') d++;
-        Print(u->r);
-    }
-// LatexBegin
-};
-// LatexEnd
-const int MAXLEN = 1e6 + 5;
-int main(){
-    srand(time(NULL));
-    d = 0;
-    Treap *sol = new Treap();
-    int n; cin >> n; while (n--){
-        int op; cin >> op;
-        char input[MAXLEN];
-        int v, p, c;
-        switch (op){
-            case 1:
-                cin >> p >> input; p -= d;
-                sol->insert(input, p);
-                break;
-            case 2:
-                cin >> p >> c; p -= d, c -= d;
-                sol->remove(p, c);
-                break;
-            case 3:
-                cin >> v >> p >> c;
-                v -= d, p -= d, c -= d;
-                sol->query(v, p, c);
-                break;
-        }
-    }
-    delete sol;
+  }
 }
